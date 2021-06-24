35434cefda7c17c6f49cbb046c6890b229d38c70
leah <lthomas@confluent.io>
2021-06-24 09:59:09 -0500

    adding testing for cleaning state stores (#7726)

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
@@ -461,7 +461,7 @@ public final class KsqlRestApplication implements Executable {
     localCommands.ifPresent(lc -> lc.processLocalCommandFiles(serviceContext));
   }
 
-  private void cleanupOldStateDirectories(KsqlConfig configWithApplicationServer) {
+  public void cleanupOldStateDirectories(final KsqlConfig configWithApplicationServer) {
     final String stateDir =
         configWithApplicationServer
             .getKsqlStreamConfigProps()
@@ -490,7 +490,7 @@ public final class KsqlRestApplication implements Executable {
                 }
               });
     } catch (IOException e) {
-      log.error("Filed to clean a state directory {}", stateDir);
+      log.error("Failed to clean a state directory {}", stateDir);
     }
   }
 
